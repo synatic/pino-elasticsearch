efb085cd5af5665d0d63f570104c942ffde2282a
Ryan Kotzen <potz666@gmail.com>
2021-09-27 12:40:11 +0200

    Formatting

--- lib.js
+++ lib.js
@@ -1,108 +1,116 @@
-'use strict'
+"use strict";
 
 /* eslint no-prototype-builtins: 0 */
 
-const split = require('split2')
-const { Client, Connection } = require('@elastic/elasticsearch')
+const split = require("split2");
+const { Client, Connection } = require("@elastic/elasticsearch");
 
-function pinoElasticSearch (opts) {
-  if (opts['bulk-size']) {
-    process.emitWarning('The "bulk-size" option has been deprecated, "flush-bytes" instead')
-    delete opts['bulk-size']
+function pinoElasticSearch(opts) {
+  if (opts["bulk-size"]) {
+    process.emitWarning(
+      'The "bulk-size" option has been deprecated, "flush-bytes" instead'
+    );
+    delete opts["bulk-size"];
   }
 
-  const splitter = split(function (line) {
-    let value
+  const splitter = split(
+    function (line) {
+      let value;
 
-    try {
-      value = JSON.parse(line)
-    } catch (error) {
-      this.emit('unknown', line, error)
-      return
-    }
-
-    if (typeof value === 'boolean') {
-      this.emit('unknown', line, 'Boolean value ignored')
-      return
-    }
-    if (value === null) {
-      this.emit('unknown', line, 'Null value ignored')
-      return
-    }
-    if (typeof value !== 'object') {
-      value = {
-        data: value,
-        time: setDateTimeString(value)
+      try {
+        value = JSON.parse(line);
+      } catch (error) {
+        this.emit("unknown", line, error);
+        return;
       }
-    } else {
-      if (value['@timestamp'] === undefined) {
-        value.time = setDateTimeString(value)
-      }
-    }
 
-    function setDateTimeString (value) {
-      if (typeof value === 'object' && value.hasOwnProperty('time')) {
-        if (
-          (typeof value.time === 'string' && value.time.length) ||
-          (typeof value.time === 'number' && value.time >= 0)
-        ) {
-          return new Date(value.time).toISOString()
+      if (typeof value === "boolean") {
+        this.emit("unknown", line, "Boolean value ignored");
+        return;
+      }
+      if (value === null) {
+        this.emit("unknown", line, "Null value ignored");
+        return;
+      }
+      if (typeof value !== "object") {
+        value = {
+          data: value,
+          time: setDateTimeString(value),
+        };
+      } else {
+        if (value["@timestamp"] === undefined) {
+          value.time = setDateTimeString(value);
         }
       }
-      return new Date().toISOString()
-    }
-    return value
-  }, { autoDestroy: true })
+
+      function setDateTimeString(value) {
+        if (typeof value === "object" && value.hasOwnProperty("time")) {
+          if (
+            (typeof value.time === "string" && value.time.length) ||
+            (typeof value.time === "number" && value.time >= 0)
+          ) {
+            return new Date(value.time).toISOString();
+          }
+        }
+        return new Date().toISOString();
+      }
+      return value;
+    },
+    { autoDestroy: true }
+  );
 
   const client = new Client({
     node: opts.node,
     auth: opts.auth,
     cloud: opts.cloud,
     ssl: { rejectUnauthorized: opts.rejectUnauthorized },
-    Connection: opts.Connection || Connection
-  })
+    Connection: opts.Connection || Connection,
+  });
 
-  const esVersion = Number(opts['es-version']) || 7
-  const index = opts.index || 'pino'
-  const buildIndexName = typeof index === 'function' ? index : null
-  const type = esVersion >= 7 ? undefined : (opts.type || 'log')
+  const esVersion = Number(opts["es-version"]) || 7;
+  const index = opts.index || "pino";
+  const buildIndexName = typeof index === "function" ? index : null;
+  const type = esVersion >= 7 ? undefined : opts.type || "log";
   const b = client.helpers.bulk({
     datasource: splitter,
-    flushBytes: opts['flush-bytes'] || 1000,
-    flushInterval: opts['flush-interval'] || 30000,
+    flushBytes: opts["flush-bytes"] || 1000,
+    flushInterval: opts["flush-interval"] || 30000,
     refreshOnCompletion: getIndexName(),
-    onDocument (doc) {
+    onDocument(doc) {
       return {
         index: {
-          _index: getIndexName(doc.time || doc['@timestamp']),
-          _type: type
-        }
-      }
+          _index: getIndexName(doc.time || doc["@timestamp"]),
+          _type: type,
+        },
+      };
     },
-    onDrop (doc) {
-      const error = new Error('Dropped document')
-      error.document = doc
-      splitter.emit('insertError', error)
-    }
-  })
+    onDrop(doc) {
+      const error = new Error("Dropped document");
+      error.document = doc;
+      splitter.emit("insertError", error);
+    },
+  });
 
   b.then(
-    (stats) => splitter.emit('insert', stats),
-    (err) => splitter.emit('error', err)
-  )
+    (stats) => splitter.emit("insert", stats),
+    (err) => splitter.emit("error", err)
+  );
 
   splitter._destroy = function (err, cb) {
-    b.then(() => cb(err), (e2) => cb(e2 || err))
-  }
+    b.then(
+      () => cb(err),
+      (e2) => cb(e2 || err)
+    );
+  };
 
-  function getIndexName (time = new Date().toISOString()) {
+  function getIndexName(time = new Date().toISOString()) {
     if (buildIndexName) {
-      return buildIndexName(time)
+      return buildIndexName(time);
     }
-    return index.replace('%{DATE}', time.substring(0, 10))
+    return index.replace("%{DATE}", time.substring(0, 10));
   }
 
-  return splitter
+  return splitter;
 }
 
-module.exports = pinoElasticSearch
+module.exports = pinoElasticSearch;
